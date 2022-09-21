c51a76228216d5c4d2410873187f5354ad41d9bf
Alex X <alex@10-19-141-0.dynapool.wireless.nyu.edu>
2022-09-21 12:50:56 -0400

    commit

--- sketch.js
+++ sketch.js
@@ -4,11 +4,12 @@ let resumeButton;
 let population;
 let lifespan = 200;
 let popsize = 25;
-let mutationrate = 0.01;
+let mutationrate = 0.02;
 let count = 0;
 let target;
 let displayinfo;
 let generation = 1;
+let lifeCycle;
 
 function setup() {
   createCanvas(400, 400);
@@ -18,7 +19,7 @@ function setup() {
   lifeCycle.class("stats");
   displayinfo = createP();
   displayinfo.class("stats");
-  target = createVector(width/2,50);
+  target = createVector(width / 2, 50);
   frameRate(100);
 }
 
@@ -27,10 +28,10 @@ function draw() {
   population.run();
   lifeCycle.html(`Current Life Span: ${count}`);
   count++;
-  if(count == lifespan){
+  if (count == lifespan) {
     population.evaluate();
     population.selection();
-    count=0;
+    count = 0;
     generation++;
   }
   drawTarget();
@@ -38,23 +39,23 @@ function draw() {
 
 
 
-function drawTarget(){
-  fill(240,204,250);
-  ellipse(target.x,target.y,20,20);
-  fill(50,183,182);
-  ellipse(target.x,target.y,10,10);
-  
+function drawTarget() {
+  fill(240, 204, 250);
+  ellipse(target.x, target.y, 20, 20);
+  fill(50, 183, 182);
+  ellipse(target.x, target.y, 10, 10);
+
 }
 
-function createButtons(){
+function createButtons() {
   stopButton = createButton("Stop");
   stopButton.position(420, 10);
-  stopButton.mousePressed(()=>{
+  stopButton.mousePressed(() => {
     noLoop();
   })
   resumeButton = createButton("Resume");
-  resumeButton.position(420,80);
-  resumeButton.mousePressed(()=>{
+  resumeButton.position(420, 80);
+  resumeButton.mousePressed(() => {
     loop();
   })
 }
